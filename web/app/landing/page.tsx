88b9a3660902ac966be4518848995f4f5107b5c5
25lp5669 User <25lp5669@G304-CC0217.local>
2026-06-25 23:03:56 +0800

    add about section

--- web/app/landing/page.tsx
+++ web/app/landing/page.tsx
@@ -1,11 +1,13 @@
 import Header from "./Header";
-
+import Hero from "./Hero";
+import About from "./About";
 
 export default function LandingPage() {
   return (
     <main className="bg-[#f8f8f8]">
       <Header />
-
+      <Hero />
+      <About />
     </main>
   );
 }

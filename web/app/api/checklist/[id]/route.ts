10b0d714ac59757d7a04b6a7904a4d6a7ece7571
Namuk nazu <nazu@Namuks-MacBook-Air.local>
2026-06-28 01:15:06 +0800

    checklist controller done

---
+++ web/app/api/checklist/[id]/route.ts
@@ -0,0 +1,72 @@
+import { prisma } from "@/lib/prisma";
+import jwt from "jsonwebtoken";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function PATCH(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const token = request.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json(
+        { error: "Нэвтрэх токен олдсонгүй" },
+        { status: 401 },
+      );
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET!);
+    const itemId = params.id;
+
+    const { isCompleted, title } = await request.json();
+
+    const updatedItem = await prisma.checklist.update({
+      where: { id: itemId },
+      data: {
+        ...(isCompleted !== undefined && { isCompleted }),
+        ...(title && { title }),
+      },
+    });
+
+    return NextResponse.json({ success: true, item: updatedItem });
+  } catch (error) {
+    console.error("Checklist PATCH Error:", error);
+    return NextResponse.json(
+      { error: "Сервер дээр алдаа гарлаа" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  try {
+    const token = request.cookies.get("token")?.value;
+    if (!token) {
+      return NextResponse.json(
+        { error: "Нэвтрэх токен олдсонгүй" },
+        { status: 401 },
+      );
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET!);
+    const itemId = params.id;
+
+    await prisma.checklist.delete({
+      where: { id: itemId },
+    });
+
+    return NextResponse.json({
+      success: true,
+      message: "Амжилттай устгагдлаа",
+    });
+  } catch (error) {
+    console.error("Checklist DELETE Error:", error);
+    return NextResponse.json(
+      { error: "Сервер дээр алдаа гарлаа" },
+      { status: 500 },
+    );
+  }
+}
